26a636a83d6a41b4081ee91ea23ba8fd6194b1d3
Ajay <anayakg1@gmail.com>
2025-02-04 21:06:44 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import cookieParser from "cookie-parser";
+import {connectDB} from "./lib/db.js";
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+
+app.listen(5001,()=>{
+    console.log("server is running on port:"+ PORT);
+    connectDB();
+})
