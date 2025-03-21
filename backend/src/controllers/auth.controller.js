fb19263d043e07c10877dbca5573efbcb7ba069a
Ajay <anayakg1@gmail.com>
2025-03-21 18:27:47 +0530

    deployment ready

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/util.js";
 import User from "../models/user.model.js";
-
+import cloudinary from "../lib/cloudinary.js";
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
     try {
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,22 +3,41 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
+
+import path from "path";
 import {connectDB} from "./lib/db.js";
 
-const app = express();
+import {app,server} from "./lib/socket.js"
+
+import cors from "cors";
+
+
+// const app = express();
+app.use(express.json({ limit: "50mb" }));  // Set a higher limit for JSON payloads
+app.use(express.urlencoded({ limit: "50mb", extended: true }));  // Same for URL-encoded data
 
 dotenv.config();
 
 const PORT = process.env.PORT
-
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true,
+}))
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
-app.listen(5001,()=>{
+server.listen(PORT,()=>{
     console.log("server is running on port:"+ PORT);
     connectDB();
 })
